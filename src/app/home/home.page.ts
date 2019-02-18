f94f7af424f117c4bad3dfc4634e89e79293919b
Andrés García <andres.ggarcia@mercadolibre.com>
2019-02-18 17:43:46 -0300

    Vista detalle [No sirve todavia]

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
+
 import { NotesService } from './../../services/notes.service';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,8 +11,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  valor = 'Prueba';
   notes = [];
-constructor(public notesService : NotesService) {
+  @ViewChild('myNav') nav: NavController
+constructor(public navCtrl: NavController, public notesService : NotesService, private router: Router) {
     this.notes = notesService.getNotes();
+  }
+
+  pushDetail(){
+    this.router.navigateByUrl(`/detail/${this.valor}`);
+  }
+
+  public goToDetail(){
+    this.navCtrl.navigateForward('/detail');
   }
 }
--- src/services/notes.service.ts
+++ src/services/notes.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class NotesService {
   notes = [
-    { id: 1, title: 'Nota 1', description: 'Descripcion de Nota 1' },
-    { id: 2, title: 'Nota 2', description: 'Descripcion de Nota 2' },
+    { id: 1, title: 'Nota Uno', description: 'Descripcion de Nota 1' },
+    { id: 2, title: 'Nota Dos', description: 'Descripcion de Nota 2' },
     { id: 3, title: 'Nota 3', description: 'Descripcion de Nota 3' }
   ];
   public getNotes() {
